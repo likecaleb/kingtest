d0aabb5dad1fa52c8be75a2016f60469593f5395
likecaleb <68595023+likecaleb@users.noreply.github.com>
2020-08-24 23:07:34 +0900

    Update qna.js

--- qna.js
+++ qna.js
@@ -5,12 +5,12 @@ function check(){
         form.action = "intp.html";
     }
     
-    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
+    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
         
         form.action = "infp.html";
     } 
 
-    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
+    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
 
         form.action = "intj.html";
     }
@@ -25,12 +25,12 @@ function check(){
         form.action = "istp.html";
     }
 
-    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
+    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
         
         form.action = "isfp.html";
     }
 
-    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
+    else if((Number(form.firstmeet[0].checked) + Number(form.freetime[1].checked) + Number(form.hidden[1].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
 
         form.action = "istj.html";
     }
@@ -45,12 +45,12 @@ function check(){
         form.action = "entp.html";
     }
 
-    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
+    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
 
         form.action = "enfp.html";
     }
 
-    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
+    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[0].checked) + Number(form.rule[1].checked) + Number(form.pill[1].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
 
         form.action = "entj.html";
     }
@@ -65,12 +65,12 @@ function check(){
         form.action = "estp.html";
     }
 
-    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
+    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
 
         form.action = "esfp.html";
     }
     
-    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[0].checked) + Number(form.day[1].checked) + Number(form.fight[1].checked) >= 2)&&(Number(form.choice[1].checked) + Number(form.happen[0].checked) + Number(form.glasses[0].checked) >= 2)){
+    else if((Number(form.firstmeet[1].checked) + Number(form.freetime[0].checked) + Number(form.hidden[0].checked) >= 2)&&(Number(form.servant[1].checked) + Number(form.rule[0].checked) + Number(form.pill[0].checked) >= 2)&&(Number(form.travel[1].checked) + Number(form.day[0].checked) + Number(form.fight[0].checked) >= 2)&&(Number(form.choice[0].checked) + Number(form.happen[1].checked) + Number(form.glasses[1].checked) >= 2)){
 
         form.action = "estj.html";
     }
